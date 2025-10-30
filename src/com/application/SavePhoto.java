0fb5fad13fbbee1ba76d13a06bb9c4ef2ab0a755
AngelAmenze <aa2551@kent.ac.uk>
2025-10-30 02:34:21 +0000

    save class created. idea - checking image is saved.

---
+++ src/com/application/SavePhoto.java
@@ -0,0 +1,18 @@
+package com.application;
+import com.utils.BitMapImage;
+import com.utils.ImageRW;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SavePhoto {
+
+    public boolean saveImage(BitMapImage image) {
+        return saveImageWithName(image, null);
+       
+    }
+   // public boolean saveImageWithName(BitMapImage image, String name) {
+        
+   // }
+}
